33e4747fb844afa0a4fa137d82b5e33941e2e7d1
jordon266 <jordon.j.murray@torontomu.ca>
2025-03-26 01:44:09 -0400

    fixedBlankUserClass

---
+++ BookStore/src/bookstore/User.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bookstore;
+import java.util.ArrayList;
+/**
+ *
+ * @author jjmurray
+ */
+public abstract class User {
+    private String username;
+    private String password;
+    User(String uname, String pword){
+        username = uname;
+        password = pword;
+    }
+    abstract ArrayList<String> viewBooks(ArrayList<String> Books);
+    public String getName(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    @Override
+    public boolean equals(Object comparedObject){
+        if(this == comparedObject){
+            return true;
+        }
+        if(!(comparedObject instanceof User)){
+            return false;
+        }
+        User comparedUser = (User) comparedObject;
+        return (this.getName().equals(comparedUser.getName())
+                && this.getPassword().equals(comparedUser.getPassword()));
+    }
+}
